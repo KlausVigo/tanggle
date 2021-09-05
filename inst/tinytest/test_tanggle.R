a17c329a1ff52f2a874a96fecadd8a0d0b0add8d
Klaus Schliep <klaus.schliep@gmail.com>
2021-09-05 11:21:30 +0200

    replace class with is, clean up

--- inst/tinytest/test_tanggle.R
+++ inst/tinytest/test_tanggle.R
@@ -2,22 +2,20 @@ library(tanggle)
 library(ape)
 
 enet <- ape::read.evonet(text="((a:2,(b:1)#H1:1):1,(#H1,c:1):2);")
-# ggevonet(enet) + geom_tiplab()
 
 expect_true(inherits(enet, "evonet"))
 min_net <- minimize_overlap(enet)
 expect_true(inherits(min_net, "evonet"))
 
-nd <- node.depth.evonet(enet)
+nd <- node_depth_evonet(enet)
 
 fe <- tanggle:::fortify.evonet(enet)
-expect_equal(class(fe), "data.frame")
+expect_true(is(fe, "data.frame"))
 
 
 data(yeast, package="phangorn")
 dm <- phangorn::dist.ml(yeast)
 nnet <- phangorn::neighborNet(dm)
-# ggsplitnet(nnet) + geom_tiplab2()
 
 fspl <- tanggle:::fortify.networx(nnet)
 expect_equal(class(fspl), "data.frame")
