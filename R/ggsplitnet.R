838f3629a0fb9f7a39a1545880741cd6d4d87f38
Klaus Schliep <klaus.schliep@gmail.com>
2022-04-12 13:22:11 +0200

    Import functions explicit with :: operator

--- R/ggsplitnet.R
+++ R/ggsplitnet.R
@@ -1,5 +1,4 @@
 #' @method fortify networx
-#' @importFrom phangorn getRoot
 #' @export
 fortify.networx <- function(model, data, layout = "unrooted", ladderize = FALSE,
                             right = FALSE, mrsd = NULL, as.Date = FALSE, ...) {
@@ -18,7 +17,7 @@ fortify.networx <- function(model, data, layout = "unrooted", ladderize = FALSE,
                     branch.length = model$edge.length,
                     split = model$splitIndex, label = label, isTip = isTip)
     if (!is.null(model$.plot)) coord <- model$.plot$vertices
-    else coord <- coords(model, dim = "equal_angle")
+    else coord <- phangorn::coords(model, dim = "equal_angle")
     df <- cbind(df, x = coord[df$node, 1], y = coord[df$node, 2],
                 xend = coord[df$parent, 1], yend = coord[df$parent, 2])
     angle <- atan2(df$y - df$yend, df$x - df$xend) * 360/(2 * pi)
